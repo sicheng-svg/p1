5c15e3f165fe11a6f7e5c8a8e656b090b4b2fa14
sichengxia <sichengxia66@gmail.com>
2026-02-24 20:48:37 +0800

    CMake

---
+++ CMake/cmaketools/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/xsc/workspace/code/cpp/p1/CMake/cmaketools
+# Build directory: /home/xsc/workspace/code/cpp/p1/CMake/cmaketools/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_add "/home/xsc/workspace/code/cpp/p1/CMake/cmaketools/build/main")
+set_tests_properties(test_add PROPERTIES  _BACKTRACE_TRIPLES "/home/xsc/workspace/code/cpp/p1/CMake/cmaketools/CMakeLists.txt;12;add_test;/home/xsc/workspace/code/cpp/p1/CMake/cmaketools/CMakeLists.txt;0;")
---
+++ CMake/cmaketools/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cassert>
+
+int main(){
+    assert(1+1 == 2);
+    // assert(1+1>3);
+    std::cout << "test ok!\n";
+    return 0;
+}
