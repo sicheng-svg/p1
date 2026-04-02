2d779de488eb93aeb96da067e917ddcf79a8e4dd
sichengxia <sichengxia66@gmail.com>
2026-04-02 21:40:10 +0800

    leetcode day 20

---
+++ leetcode/2604.cc
@@ -0,0 +1,38 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, right = 0, len(nums)-1
+        i = 0
+        while i <= right:
+            if nums[i] == 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                i += 1
+                left += 1
+            elif nums[i] == 1:
+                i += 1
+            else:
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
+
+                class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0, count = 0;
+        for(int val:nums){
+            if(count == 0){
+                candidate = val;
+            }
+            count = candidate == val ? count+1: count-1;
+        }
+        return candidate;
+    }
+};
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for i in nums:
+            ans ^= i
+        return ans
