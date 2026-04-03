feeb8d93e64057a1e3d237d3fef4a67be4dd853f
sichengxia <sichengxia66@gmail.com>
2026-04-03 20:49:48 +0800

    leetcode day21

--- leetcode/2604.cc
+++ leetcode/2604.cc
@@ -36,3 +36,22 @@ class Solution:
         for i in nums:
             ans ^= i
         return ans
+
+        class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j = n - 1;
+            while(nums[j] <= nums[i]) {
+                j--;
+            }
+            std::swap(nums[i], nums[j]);
+        }
+        std::sort(nums.begin() + i + 1, nums.end());
+    }
+};
