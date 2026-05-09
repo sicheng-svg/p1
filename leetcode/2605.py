6ed9a6dd64fdc7e44562a012b3b4edca8a094621
sichengxia <sichengxia66@gmail.com>
2026-05-09 17:18:06 +0800

    donggui

--- leetcode/2605.py
+++ leetcode/2605.py
@@ -31,3 +31,27 @@ class Solution:
                 jumps += 1
                 end = farthest
         return jumps
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_index = {c: i for i, c in enumerate(s)}
+
+        result = []
+        start = end = 0
+        for i, c in enumerate(s):
+            end = max(end, last_index[c])
+            if i == end:
+                result.append(i - start + 1)
+                start = i + 1
+        return result
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # dp[i][j] 表示达到该位置是一共有多少种走法
+        # dp[i][j] = dp[i][j-1] + dp[i-1][j]
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        dp[0][1] = 1
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m][n]
