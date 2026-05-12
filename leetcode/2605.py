c0150f4f743b944525056387528bcbb550d5ed35
sichengxia <sichengxia66@gmail.com>
2026-05-12 14:40:49 +0800

    接雨水

--- leetcode/2605.py
+++ leetcode/2605.py
@@ -85,3 +85,17 @@ class Solution:
     
         return s[start:start+max_len]
         
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        leftMax, rightMax, ans = 0, 0, 0
+        while left < right:
+            if height[left] < height[right]:
+                leftMax = max(leftMax, height[left])
+                ans += leftMax - height[left]
+                left += 1
+            else:
+                rightMax = max(rightMax, height[right])
+                ans += rightMax - height[right]
+                right -= 1
+        return ans
