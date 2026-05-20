dc9c095de34233f30f5c806d51bd45ad4d3e2615
sichengxia <sichengxia66@gmail.com>
2026-05-20 21:25:30 +0800

    leetcode

--- leetcode/2605.py
+++ leetcode/2605.py
@@ -199,3 +199,19 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        std::vector<int> cnt(n+1, 0);
+        std::vector<int> ans(n);
+        int common = 0;
+        for(int i=0; i<n; ++i){
+            if(++cnt[A[i]] == 2) common++;
+            if(++cnt[B[i]] == 2) common++;
+            ans[i] = common;
+        }
+        return ans;
+    }
+};
