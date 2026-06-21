133d68f0a8068935f125fd613a6a7a55fb12a916
sichengxia <sichengxia66@gmail.com>
2026-06-21 23:34:05 +0800

    leetcode

--- leetcode/2606.cc
+++ leetcode/2606.cc
@@ -2299,3 +2299,22 @@ public:
         return root;
     }
 };
+
+class Solution86 {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int count = 0;
+        int n = costs.size();
+        for (int i = 0; i < n; i++) {
+            int cost = costs[i];
+            if (coins >= cost) {
+                coins -= cost;
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+};
