cf5e8d0fd7809c548593349da940dc504aa304ad
sichengxia <sichengxia66@gmail.com>
2026-07-03 23:34:06 +0800

    leetcode

--- leetcode/2607.cc
+++ leetcode/2607.cc
@@ -3,6 +3,7 @@
 #include <queue>
 #include <stack>
 #include <climits>
+#include <algorithm>
 using namespace std;
 
 class Solution1 {
@@ -206,5 +207,56 @@ public:
         }
 
         return dis[m - 1][n - 1] < health;
+    }
+
+
+};
+
+class Solution8 {
+public:
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+        int n = online.size();
+        vector<vector<pair<int, int>>> g(n);
+        int max_wt = -1;
+        for (auto& e : edges) {
+            int x = e[0], y = e[1], wt = e[2];
+            if (online[x] && online[y]) {
+                g[x].emplace_back(y, wt);
+                if (x == 0) {
+                    max_wt = max(max_wt, wt);
+                }
+            }
+        }
+
+        vector<long long> memo(n);
+        auto check = [&](int lower) -> bool {
+            ranges::fill(memo, -1); // -1 表示没有计算过
+
+            auto dfs = [&](this auto&& dfs, int x) -> long long {
+                if (x == n - 1) { // 到达终点
+                    return 0;
+                }
+                auto& res = memo[x]; // 注意这里是引用
+                if (res != -1) { // 之前计算过
+                    return memo[x];
+                }
+                res = LLONG_MAX / 2; // 防止加法溢出
+                for (auto& [y, wt] : g[x]) {
+                    if (wt >= lower) {
+                        res = min(res, dfs(y) + wt);
+                    }
+                }
+                return res;
+            };
+
+            return dfs(0) <= k;
+        };
+
+        int left = -1, right = max_wt + 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            (check(mid) ? left : right) = mid;
+        }
+        return left;
     }
 };
