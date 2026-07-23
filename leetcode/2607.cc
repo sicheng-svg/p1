46852acb33d1db2ef73640dc89107be57cf42aaa
sichengxia <sichengxia66@gmail.com>
2026-07-23 17:33:09 +0800

    leetcode

--- leetcode/2607.cc
+++ leetcode/2607.cc
@@ -838,3 +838,15 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 2) return n;
+        
+        int ans = 1;
+        while(ans <= n) ans <<= 1;
+        return ans;
+    }
+};
